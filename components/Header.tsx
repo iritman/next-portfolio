1da342684e053c1b8ab4bf624708fa76c9d1c22a
Naiem YF <iritman@gmail.com>
2025-01-27 21:18:49 +0330

    Create Navbar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,32 @@
-import { Text } from '@radix-ui/themes'
+import { Box, Button, Flex, Text } from '@radix-ui/themes'
 import Link from 'next/link'
-import React from 'react'
+import Navbar from './Navbar'
 
 const Header = () => {
     return (
-        <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-            <Link href="/">
-                <Text size="6" weight="bold">
-                    Naiem
-                    <Text size="6" weight="bold" color="cyan">.</Text>
-                    YF
-                </Text>
-            </Link>
+        <header className="py-8 xl:py-12 text-white">
+            <Flex justify="between" align="center">
+                <Link href="/">
+                    <Text size="6" weight="bold">
+                        Naiem
+                        <Text size="6" weight="bold" color="cyan">.</Text>
+                        YF
+                    </Text>
+                </Link>
+
+                {/* Desktop nav */}
+                <div className='hidden xl:flex items-center gap-8'>
+                    <Navbar />
+                    <Link href="/contact">
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile nav */}
+                <div className='xl:hidden'>
+                    Mobile nav
+                </div>
+            </Flex>
         </header>
     )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Flex } from "@radix-ui/themes"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const links = [
+    { name: "home", path: "/" },
+    { name: "services", path: "/services" },
+    { name: "resume", path: "/resume" },
+    { name: "work", path: "/work" },
+    { name: "contact", path: "/contact" },
+]
+
+const Navbar = () => {
+    const pathname = usePathname();
+
+    return (
+        <Flex gap="4">
+            {links.map((link, index) => (
+                <Link
+                    href={link.path}
+                    key={index}
+                    className={
+                        `${link.path === pathname && "text-cyan-300 border-b-2 border-cyan-300"}
+                        capitalize font-medium hover:text-cyan-300 transition-all`}>
+                    {link.name}
+                </Link>
+            ))}
+        </Flex>
+    )
+}
+
+export default Navbar
