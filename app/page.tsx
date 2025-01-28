4e03008286851ef540a5f36def642f74de1514ab
Naiem YF <iritman@gmail.com>
2025-01-28 15:32:56 +0330

    Inital design of Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
-export default function Home() {
+import { Button, Container, Flex, Text } from '@radix-ui/themes'
+import { FiDownload as DownloadIcon } from 'react-icons/fi'
+
+const HomePage = () => {
   return (
-    <main>Home page</main>
-  );
+    <section className="h-full">
+      <Container>
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <Flex direction="column">
+              <Text size="2">Software Engineer</Text>
+              <Text size="7">Hello I'm</Text>
+              <Text mb="6" size="9" className='text-[var(--highlight)]'>Naiem YF</Text>
+              <Text mb="9" className='max-w-[25rem]'>
+                I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies
+              </Text>
+
+              <div className='flex flex-col xl:flex-row items-center gap-8'>
+
+                <Button color="cyan" radius='full'>
+                  Download CV <DownloadIcon />
+                </Button>
+
+                <div className='mb-8 xl:mb-0'>
+                  Socials
+                </div>
+              </div>
+            </Flex>
+          </div>
+          <div>photo</div>
+        </div>
+      </Container>
+    </section>
+  )
 }
+
+export default HomePage
